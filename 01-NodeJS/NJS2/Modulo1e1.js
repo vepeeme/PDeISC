14b08cfe58fbd9d9cf4d4cc49b98cfe449f176ff
vepeeme <palmamartorellovalentino@gmail.com>
2025-04-16 20:23:23 -0300

    Proyecto

---
+++ 01-NodeJS/NJS2/Modulo1e1.js
@@ -0,0 +1,16 @@
+//exporta una funcion que suma a y b
+export function sumar(a,b) {
+    return a + b;
+}
+//exporta una funcion que resta a y b
+export function restar(a,b) {
+    return a - b;
+}
+//exporta una funcion que multiplica a y b
+export function multiplicar(a,b) {
+    return a * b;
+}
+//exporta una funcion que division a y b
+export function dividir(a,b) {
+    return a / b;
+}
